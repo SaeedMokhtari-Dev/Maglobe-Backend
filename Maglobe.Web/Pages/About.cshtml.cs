bc8e9c83fbc04b8385150dd3be0880460223fbce
Deep <mpds93@yahoo.com>
2021-12-27 13:22:17 +0330

    Add About Page

---
+++ Maglobe.Web/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Maglobe.Core.Enums;
+using Maglobe.Web.Pages.Shared;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Maglobe.Web.Pages
+{
+    [AllowAnonymous]
+    public class About : BasePageModel
+    {
+        public About()
+        {
+        }
+        public async Task OnGet(string siteLanguage)
+        {
+            Language language = CheckAndSetSiteLanguage(siteLanguage);
+            ViewData["BodyClass"] = "about";
+        }
+    }
+}
--- Maglobe.Web/Pages/Products.cshtml.cs
+++ Maglobe.Web/Pages/Products.cshtml.cs
@@ -19,6 +19,8 @@ namespace Maglobe.Web.Pages
             Language language = CheckAndSetSiteLanguage(siteLanguage);
             
             ViewData["Products"] = await _productService.GetActiveProducts(language);
+            ViewData["BodyClass"] = "products";
+
         }
     }
 }
